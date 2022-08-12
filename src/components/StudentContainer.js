4f799cb799189f7cb5db1f985ea9ef9139a4b3c8
hybeesalam <hybeesalam@gmail.com>
2022-08-12 13:51:58 +0100

    'blah'

---
+++ src/components/StudentContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class StudentContainer extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>Hello from Torillo world</h1>
+                <p>I am the parent component</p>
+            </div>
+        )
+
+    }
+}
+export default StudentContainer;
